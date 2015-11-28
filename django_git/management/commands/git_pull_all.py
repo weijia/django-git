68b96a3556484eebb525141bd0d9c7439412c4f8
Richard <richardwangwang@gmail.com>
2015-11-28 13:26:56 +0800

    Added drop to pull.

--- django_git/management/commands/git_pull_all.py
+++ django_git/management/commands/git_pull_all.py
@@ -1,27 +1,33 @@
-from universal_clipboard.management.commands.cmd_handler_base.msg_process_cmd_base import MsgProcessCommandBase
+from UserDict import UserDict
 
-__author__ = 'weijia'
-
-
-import json
-
-# # from background_task.tasks import tasks, autodiscover
 from iconizer.iconizer_consts import ICONIZER_SERVICE_NAME
-from obj_sys.models_ufs_obj import UfsObj
+from iconizer.msg_service.msg_def.file_url_list_msg import FileUrlListMsg
+from universal_clipboard.management.commands.cmd_handler_base.msg_process_cmd_base import MsgProcessCommandBase
+from django_git.management.commands.git_pull_utils.puller import Puller
+
+
+def get_full_path_from_url(url):
+    return url.replace("file:///", "")
 
 
 class GitMsgHandler(MsgProcessCommandBase):
     def register_to_service(self):
-        clipboard_receive_channel_name = "clipboard_to_django"
-        channel = self.ufs_msg_service.create_channel(clipboard_receive_channel_name)
-        self.ufs_msg_service.send_to(ICONIZER_SERVICE_NAME, {"command": "register_to_clipboard",
-                                                     "target": channel.get_channel_full_name()})
+        channel = self.get_channel("git_puller")
+        reg_msg = UserDict({"command": "DropWndV2", "tip": "GIT test", "target": channel.get_channel_full_name()})
+        self.ufs_msg_service.send_to(ICONIZER_SERVICE_NAME, reg_msg.data)
         return channel
 
     # noinspection PyMethodMayBeStatic
     def process_msg(self, msg):
-        clipboard_msg = {"msg_type": "clipboard", "data": msg}
-        UfsObj.objects.get_or_create(description_json=json.dumps(clipboard_msg["data"]), ufs_obj_type=3)
+        file_msg = FileUrlListMsg(msg)
+        file_url_list = file_msg.get_file_url_list()
+        for url in file_url_list:
+            # path = msg["path"]
+            # path will be in file://xxxxx format
+            path = get_full_path_from_url(url)
+            print "processing:", path
+            p = Puller(path)
+            p.pull_all()
 
 
 Command = GitMsgHandler
