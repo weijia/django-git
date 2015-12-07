c43bac62d3c34a0f724842d7a9c70c0cf0c1ffd9
Richard <richardwangwang@gmail.com>
2015-12-07 23:27:19 +0800

    Added auto pull.

--- django_git/management/commands/git_pull_all.py
+++ django_git/management/commands/git_pull_all.py
@@ -1,7 +1,14 @@
 from UserDict import UserDict
 
+import time
+
+import thread
+
+from tagging.models import Tag
+from tagging.models import TaggedItem
 from iconizer.iconizer_consts import ICONIZER_SERVICE_NAME
 from iconizer.msg_service.msg_def.file_url_list_msg import FileUrlListMsg
+from obj_sys.obj_tagging import get_or_create_objects_from_remote_or_local_url, append_tags_and_description_to_url
 from universal_clipboard.management.commands.cmd_handler_base.msg_process_cmd_base import MsgProcessCommandBase
 from django_git.management.commands.git_pull_utils.puller import Puller
 
@@ -10,11 +17,27 @@ def get_full_path_from_url(url):
     return url.replace("file:///", "")
 
 
+def tag_enumerator(tag_name="git"):
+    while True:
+        tag = Tag.objects.get(name=tag_name)
+        tagged_item_list = TaggedItem.objects.filter(tag__exact=tag.pk)
+        for tagged_item in tagged_item_list:
+            obj_tag = tagged_item.tag.name
+            obj = tagged_item.object
+            path = obj.full_path
+            print "processing:", path
+            p = Puller(path)
+            p.pull_all()
+            print "auto pull and push done"
+        time.sleep(60*5)
+
+
 class GitMsgHandler(MsgProcessCommandBase):
     def register_to_service(self):
         channel = self.get_channel("git_puller")
         reg_msg = UserDict({"command": "DropWndV2", "tip": "GIT test", "target": channel.get_channel_full_name()})
         self.ufs_msg_service.send_to(ICONIZER_SERVICE_NAME, reg_msg.data)
+        thread.start_new_thread(tag_enumerator)
         return channel
 
     # noinspection PyMethodMayBeStatic
@@ -24,6 +47,7 @@ class GitMsgHandler(MsgProcessCommandBase):
         for url in file_url_list:
             # path = msg["path"]
             # path will be in file://xxxxx format
+            append_tags_and_description_to_url(self.admin_user, url, ["git"], "GIT repository")
             path = get_full_path_from_url(url)
             print "processing:", path
             p = Puller(path)
