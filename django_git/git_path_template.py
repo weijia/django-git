7e65deea12a8d0be69ce36ec2d6aae6bc23e2e0b
richard <richardwangwang@gmail.com>
2016-03-07 21:21:38 +0800

    Update git path template.

--- django_git/git_path_template.py
+++ django_git/git_path_template.py
@@ -1,1 +1,2 @@
-git_folder = ['C:\\Users\\weijia\\AppData\\Local\\Programs\\Git\\bin']
+git_folder = ['C:\\Users\\weijia\\AppData\\Local\\Programs\\Git\\bin', 'C:\\Program Files (x86)\\Git\\bin',
+              'C:\\Program Files\\Git\\bin']
